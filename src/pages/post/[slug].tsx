d0ae29aa144be952500d171bb9fab68fc25b9531
Caetano Burjack <caetano.burjack@gmail.com>
2022-04-09 09:15:07 -0300

    Creating post tests

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -29,6 +29,7 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  console.log(post)
   return (
     <>
       <Header />
@@ -70,16 +71,43 @@ export default function Post({ post }: PostProps) {
   )
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: true,
+  }
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient()
+  const { slug } = context.params
+  const response = await prismic.getByUID('p', String(slug), {})
 
-//   // TODO
-// };
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heanding: content.heading,
+          body: [...content.body],
+        }
+      })
+    }
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+};
