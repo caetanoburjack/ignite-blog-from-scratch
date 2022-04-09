cd962289e766b20edb1ed2750dcb9eef75c08714
Caetano Burjack <caetano.burjack@gmail.com>
2022-04-09 08:31:32 -0300

    Creating post tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import Prismic from '@prismicio/client'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 interface Post {
   uid?: string;
@@ -27,47 +30,84 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ }: HomeProps) {
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR
+        }
+      )
+    }
+  })
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
+
+    const postsResults = await fetch(`${nextPage}`).then(response =>
+      response.json())
+    setNextPage(postsResults.next_page)
+    setCurrentPage(currentPage + 1)
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author
+        }
+      }
+    })
+    setPosts([...posts, ...newPosts])
+  }
+
+
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
         <div className={styles.posts}>
-          <Link href='/'>
-            <a className={styles.post}>
-              <strong>Titulo do Post</strong>
-              <p>Subtitulo do post</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  07 de Abril de 2022
-                </li>
-                <li>
-                  <FiUser />
-                  Caetano
-                </li>
-              </ul>
-            </a>
-          </Link>
-          <Link href='/'>
-            <a className={styles.post}>
-              <strong>Titulo do Post</strong>
-              <p>Subtitulo do post</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  07 de Abril de 2022
-                </li>
-                <li>
-                  <FiUser />
-                  Caetano
-                </li>
-              </ul>
-            </a>
-          </Link>
-          <button type="button">
-            Carregar mais posts
-          </button>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
+          {nextPage &&
+            (
+              <button type="button" onClick={handleNextPage}>
+                Carregar mais posts
+              </button>
+            )}
         </div>
       </main>
     </>
@@ -99,10 +139,15 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts
+  }
+
   // console.log(postsResponse.results);
   return {
     props: {
-      postsResponse,
+      postsPagination,
     }
   }
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,47 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <img src="/banner.svg" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Titulo do post</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                07 de abril de 2022
+              </li>
+              <li>
+                <FiUser />
+                Caetano Burjack
+              </li>
+              <li>
+                <FiClock />
+                5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Titulo seção</h2>
+            <p>
+              texto falso texto falso texto falso texto falso
+              texto falso texto falso texto falso texto falso
+              texto falso texto falso texto falso texto falso
+              texto falso texto falso texto falso texto falso
+              texto falso
+            </p>
+          </article>
+
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
